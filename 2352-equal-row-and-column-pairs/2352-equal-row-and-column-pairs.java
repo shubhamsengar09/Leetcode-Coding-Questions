f64c0d75a8cb7e80d9f875a092222390849a78bd
Shubham Sengar <75666043+shubhamsengar09@users.noreply.github.com>
2022-07-24 10:42:30 +0530

    Time: 236 ms (20.00%), Space: 50.4 MB (100.00%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,31 +1,50 @@
 class Solution {
     public int equalPairs(int[][] grid) {
         int n = grid.length;
-        Map<String, Integer> mp1 = new HashMap<>();
-        Map<String, Integer> mp2 = new HashMap<>();
+        Map<String, ArrayList<Integer>> mp1 = new HashMap<>();
         for(int i =0;i<n;i++){
             String row = " ";
             for(int j =0;j<n;j++){
                 row += grid[i][j];
                 row += ",";
             }
-            mp1.put(row,mp1.getOrDefault(row,0) +1);
+           if(mp1.containsKey(row)){
+               ArrayList<Integer> val = mp1.get(row);
+               val.set(0,val.get(0)+1);
+               mp1.replace(row,val);
+           }else{
+               mp1.put(row,new ArrayList<>());
+               mp1.get(row).add(1);
+               mp1.get(row).add(0);
+               
+           }      
+        
         }
         for(int j =0;j<n;j++){
-            String col = " ";
+            String row = " ";
             for(int i =0;i<n;i++){
-                col += grid[i][j];
-                col += ",";
+                row += grid[i][j];
+                row += ",";
             }
-            mp2.put(col,mp2.getOrDefault(col,0) +1);
+           if(mp1.containsKey(row)){
+               ArrayList<Integer> val = mp1.get(row);
+                val.set(1,val.get(1)+1);
+               mp1.replace(row,val);
+           }else{
+               mp1.put(row,new ArrayList<>());
+               mp1.get(row).add(0);
+               mp1.get(row).add(1);
+               
+           }      
+        
         }
+      
         int ans = 0;
-        for(Map.Entry<String,Integer> m: mp1.entrySet()){
-            if(mp2.containsKey(m.getKey())){
-                int a = m.getValue();
-                int b = mp2.get(m.getKey());
+        for(Map.Entry<String,ArrayList<Integer>> m: mp1.entrySet()){
+                int a = m.getValue().get(0);
+                int b = m.getValue().get(1);
                 ans += (a*b);
-            }
+            
         }
         return ans;
         
