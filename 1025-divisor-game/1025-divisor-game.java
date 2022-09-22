92e4c48af95486d233639c157004171719804f98
Shubham Sengar <75666043+shubhamsengar09@users.noreply.github.com>
2022-09-22 09:40:55 +0530

    Time: 5 ms (16.83%), Space: 39.1 MB (90.87%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean divisorGame(int n) {
-        return n%2==0;
+        boolean res [] = new boolean[n+1];
+        if(n==1) return false;
+        res [1] = false;
+        res [2] = true;
+        for(int i =3;i<=n;i++){
+            res[i] = false;
+            for(int j = 1; j < i; j++){
+                if(i % j == 0 && !res[i - j]){
+                    res[i] = true;
+                    break;
+                }
+            }
+        }
+        return res[n];
     }
 }
